7ece71ff44bcf49025ccbd4203ad464f32ef20a7
bartenevaleriia <bartenevalera9@gmail.com>
2025-11-24 10:50:58 +0300

    Add files via upload

---
+++ доп3.py
@@ -0,0 +1,16 @@
+month=int(input('Введите номер месяца (1-12) '))
+if 1<=month<=12:
+    if month==1 or month==3 or month==5 or month==7 or month==8 or month==10 or month==12:
+        print('31')
+    elif month==4 or month==6 or month==9 or month==11:
+     print('30')
+    else:
+        print('28')
+else:
+    print('Некорекктный номер месяца ')
+
+
+
+
+
+
